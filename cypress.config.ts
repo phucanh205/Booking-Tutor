4081ede1cf9739dd87474f4debc55c0d7e965294
Your Name <you@example.com>
2026-06-01 10:55:25 +0700

    buggg

---
+++ cypress.config.ts
@@ -0,0 +1,11 @@
+export default {
+  projectId: 'wopbks',
+  allowCypressEnv: false,
+
+  e2e: {
+     baseUrl: 'http://localhost:3000',
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+};
